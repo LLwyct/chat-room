78949232219e7e7fc3c4f2693407b3e4f287b1c9
liwenchi123000 <787552171@qq.com>
2019-04-04 14:21:06 +0800

    nothing

---
+++ server.js
@@ -0,0 +1,18 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('a user disconnected');
+    })
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
