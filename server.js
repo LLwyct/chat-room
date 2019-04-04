e2f685c3973c149840c6d0a86353b330ac8c4de1
outsider <787552171@qq.com>
2019-04-04 21:02:27 +0800

    step 1

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
-let app = require('express')();
+let express = require('express');
+let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
+app.use(express.static('dist'))
+
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + './dist/index.html');
 });
+
+let onlinenum = 0;
 
 io.on('connection', function (socket) {
     console.log('a user connected');
+    onlinenum += 1;
+
+
+
     socket.on('disconnect', function () {
         console.log('a user disconnected');
+        onlinenum -= 1;
+    })
+
+    // 对于一个用户监听‘chat-message’，将会发送信息到所有人，包括发送者
+    socket.on('chat-message', function (msg) {
+        console.log('say:', msg.msg_content);
+        io.emit('chat-message', msg)
     })
 });
 
 http.listen(3000, function () {
-    console.log('listening on *:3000');
+    console.log('listening on: localhost:3000');
 });
